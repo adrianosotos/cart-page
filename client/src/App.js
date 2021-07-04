9356842e49ccdba949fdfe69d5a523ae1797fcee
Adriano <adriano.sotos@linx3.com>
2021-07-04 18:48:22 -0300

    feat: imported cart component

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './assets/styles/App.scss';
+import Cart from './pages/Cart'
+
+function App() {
+  return (
+    <div className="App">
+      <Cart />
+    </div>
+  );
+}
+
+export default App;
