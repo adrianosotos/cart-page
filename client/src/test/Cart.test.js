7fb6ce5ef01500856d4d434c8fd5e46912dd8905
Adriano <adriano.sotos@linx3.com>
2021-07-04 23:26:30 -0300

    refactor: removed unused variable

--- client/src/test/Cart.test.js
+++ client/src/test/Cart.test.js
@@ -1,4 +1,4 @@
-import { render, screen, act, cleanup, waitFor } from '@testing-library/react';
+import { render, screen, cleanup, waitFor } from '@testing-library/react';
 import Cart from '../pages/Cart'
 import axios from 'axios';
 
